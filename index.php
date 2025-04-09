a1ead7f79b0fead2b561e9e8231ce6c4be65fe01
JailtonJunior14 <jailtonjrbr@gmail.com>
2025-04-09 15:46:03 -0300

    .htaccess e core

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
-session_start();
+// session_start();
 
-include "config.php";
-include "autoload.php";
-include "routes.php";
+// include "config.php";
+// include "autoload.php";
+// include "routes.php";
+
+echo $_GET['url'];''
